c8f46ebb628599bab61b10c412e8a9884358f61f
Trpimir <t.rajevskij@yahoo.com>
2022-02-20 14:18:15 +0100

    finished game code, added assets
    .

--- client/src/code/components/PlayerBoard.tsx
+++ client/src/code/components/PlayerBoard.tsx
@@ -81,7 +81,7 @@ const PlayerBoard = ({ state}: any) => {
                     const yLoc: number = ((r * tileHeight) - (r * tileShadowHeight)) + yOffset + yAdjust; 
 
                     //plocica stil
-                    const syle: CSSProperties = {
+                    const style: CSSProperties = {
                         position : "absolute",
                         left : `${xLoc}px`,
                         top : `${yLoc}px`
@@ -95,9 +95,57 @@ const PlayerBoard = ({ state}: any) => {
                     }
 
                     //renderaj prikladni img tag
+                    switch (tileVal) {
+                        case 101 : tiles.push(<img style={style} src={tile101} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 102 : tiles.push(<img style={style} src={tile102} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 103 : tiles.push(<img style={style} src={tile103} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 104 : tiles.push(<img style={style} src={tile104} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 105 : tiles.push(<img style={style} src={tile105} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 106 : tiles.push(<img style={style} src={tile106} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 107 : tiles.push(<img style={style} src={tile107} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 108 : tiles.push(<img style={style} src={tile108} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 109 : tiles.push(<img style={style} src={tile109} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 110 : tiles.push(<img style={style} src={tile110} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 111 : tiles.push(<img style={style} src={tile111} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 112 : tiles.push(<img style={style} src={tile112} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 113 : tiles.push(<img style={style} src={tile113} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 114 : tiles.push(<img style={style} src={tile114} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 115 : tiles.push(<img style={style} src={tile115} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 116 : tiles.push(<img style={style} src={tile116} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 117 : tiles.push(<img style={style} src={tile117} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 118 : tiles.push(<img style={style} src={tile118} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 119 : tiles.push(<img style={style} src={tile119} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 120 : tiles.push(<img style={style} src={tile120} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 121 : tiles.push(<img style={style} src={tile121} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 122 : tiles.push(<img style={style} src={tile122} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 123 : tiles.push(<img style={style} src={tile123} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 124 : tiles.push(<img style={style} src={tile124} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 125 : tiles.push(<img style={style} src={tile125} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 126 : tiles.push(<img style={style} src={tile126} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 127 : tiles.push(<img style={style} src={tile127} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 128 : tiles.push(<img style={style} src={tile128} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 129 : tiles.push(<img style={style} src={tile129} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 130 : tiles.push(<img style={style} src={tile130} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 131 : tiles.push(<img style={style} src={tile131} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 132 : tiles.push(<img style={style} src={tile132} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 133 : tiles.push(<img style={style} src={tile133} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 134 : tiles.push(<img style={style} src={tile134} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 135 : tiles.push(<img style={style} src={tile135} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 136 : tiles.push(<img style={style} src={tile136} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 137 : tiles.push(<img style={style} src={tile137} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 138 : tiles.push(<img style={style} src={tile138} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 139 : tiles.push(<img style={style} src={tile139} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 140 : tiles.push(<img style={style} src={tile140} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 141 : tiles.push(<img style={style} src={tile141} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                        case 142 : tiles.push(<img style={style} src={tile142} className={className} onClick={()=>state.tileClick(l, r, c)} alt="" />); break;
+                      }
                 }
             }
         }
     }
 
-}
+    return (<React.Fragment>{tiles}</React.Fragment>);
+
+};
+
+export default PlayerBoard;
